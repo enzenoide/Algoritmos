08ea5d0e2d38c613e5a874712da9181220715ce0
enzenoide <henzefernandes1974@gmail.com>
2026-01-12 20:41:04 -0300

    Questão 4 da Lista 2

--- Backtracking/ListaDeEx2/4.cpp
+++ Backtracking/ListaDeEx2/4.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
 #include <vector>
-using namspace std;
-vector sublista_menor(<vector<int> lista, vector<int>)
+using namespace std;
+
+void sublista_bt(vector<int> &lista ,int indice,int soma,vector<int> &resposta,int &menor_sub,vector<int> &melhor_sub){
+    if(soma == 0){
+      if(resposta.size() < menor_sub){
+          menor_sub = resposta.size();
+          melhor_sub = resposta;
+    }
+    return;
+    }
+    if(soma < 0 || indice == lista.size()){
+        return;
+    }
+
+    resposta.push_back(lista[indice]);
+    sublista_bt(lista,indice + 1, soma - lista[indice],resposta,menor_sub,melhor_sub);
+    resposta.pop_back();
+    sublista_bt(lista,indice+1,soma,resposta,menor_sub,melhor_sub);
+  
+    return;
+}
+int main(){
+    int n,soma,indice;
+    cin >> n >> soma;
+    vector<int> lista(n);
+    for(int i = 0; i < n; ++i){
+        cin >> lista[i];
+    }
+    vector<int> resultado;
+    vector<int> melhor_sub;
+    int menor_sub = 10000000000;
+    sublista_bt(lista,0,soma,resultado,menor_sub,melhor_sub);
+    if(melhor_sub.size() == 0){
+      cout << 0;        
+    }
+    else{
+      for(int i = 0; i < melhor_sub.size();++i){
+            cout << melhor_sub[i] << " ";
+        }
+    }
+    return 0;
+}
+
