a67d3f1dde4aa0079b40c882f411b1ca9a772544
enzenoide <henzefernandes1974@gmail.com>
2026-01-29 18:05:16 -0300

    metodo de seleção

--- sort_algorithms/ordenacao.cpp
+++ sort_algorithms/ordenacao.cpp
@@ -15,10 +15,25 @@ bool ordenado(int a[],  unsigned int t){
 
 /* TODO: Implementar função */
 void selecao(int a[], unsigned int t){
+    int min, aux;
+    for(int i = 0; i < t - 1; ++i){
+      min = i;
+      for(int j = i + 1;j < t;++j){
+          if(a[j] < a[min]){
+          min = j;
+      }
+      if(a[i] != a[min]){
+          aux = a[i];
+          a[i] = a[min];
+          a[min] = aux;
+      }
+    }
+  }
 }
 
 /* TODO: Implementar função */
 void insercao(int a[], unsigned int t){
+  
 }
 
 /* TODO: Implementar função */
